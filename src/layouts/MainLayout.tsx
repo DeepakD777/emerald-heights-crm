a94ae4646c37d864985095a60321bed4471bbb94
DeepakD777 <deepakdubey91311@gmail.com>
2026-08-07 13:49:39 +0530

    Implemented nested routing with React Router and Outlet architecture

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,70 +1,26 @@
-/*
-========================================================
+import { Outlet } from "react-router-dom";
 
-Project : Emerald Heights CRM
-
-File : MainLayout.tsx
-
-Purpose :
-
-पूरी Website का Common Layout
-
-इस Layout में हमेशा रहेगा
-
-✔ Sidebar
-✔ Navbar
-✔ Main Content
-
-सिर्फ Main Content हर Page पर बदलेगा।
-
-========================================================
-*/
-
-import type { ReactNode } from "react";
-
-// Left Sidebar
 import Sidebar from "./Sidebar";
-
-// Top Navbar
 import Navbar from "../components/dashboard/Navbar";
 
-// MainLayout को कौन-कौन सी Properties मिलेंगी
-interface MainLayoutProps {
-
-  // जिस Page को दिखाना है
-  children: ReactNode;
-}
-
-// Main Layout Component
-function MainLayout({ children }: MainLayoutProps) {
-
+function MainLayout() {
   return (
-
-    // पूरी Screen को दो हिस्सों में बांट रहे हैं
     <div className="flex min-h-screen bg-gray-100">
 
-      {/* Left Sidebar */}
       <Sidebar />
 
-      {/* Right Side */}
       <div className="flex-1">
 
-        {/* Top Navbar */}
         <Navbar />
 
-        {/* हर Page का Content */}
         <main className="p-6">
-
-          {children}
-
+          <Outlet />
         </main>
 
       </div>
 
     </div>
-
   );
-
 }
 
 export default MainLayout;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,36 +1,25 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// import Dashboard from "../pages/Dashboard/Dashboard";
-
-// function AppRoutes() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Dashboard />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default AppRoutes;
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Dashboard from "../pages/Dashboard/Dashboard";
 import MainLayout from "../layouts/MainLayout";
+
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Bookings from "../components/dashboard/Bookings";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
 
-        <Route
-          path="/"
-          element={
-            <MainLayout>
-              <Dashboard />
-            </MainLayout>
-          }
-        />
+        <Route path="/" element={<MainLayout />}>
+
+          <Route index element={<Dashboard />} />
+
+          <Route
+            path="bookings"
+            element={<Bookings />}
+          />
+
+        </Route>
 
       </Routes>
     </BrowserRouter>
